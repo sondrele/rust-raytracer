8c6abf8bc833259b2a3039ae698505909ccc3779
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-10-10 10:50:22 +0200

    Added a Cell to Ray that tells wether it is in vacuum or not

--- src/ray.rs
+++ src/ray.rs
@@ -1,23 +1,39 @@
+use std::cell::Cell;
+
 use vec::Vec3;
 
 pub struct Ray {
     pub ori: Vec3,
-    pub dir: Vec3
+    pub dir: Vec3,
+    vacuum: Cell<bool>
 }
 
 impl Ray {
     pub fn new() -> Ray {
         Ray {
             ori: Vec3::new(),
-            dir: Vec3::new()
+            dir: Vec3::new(),
+            vacuum: Cell::new(true)
         }
     }
 
     pub fn init(ori: Vec3, dir: Vec3) -> Ray {
         Ray {
             ori: ori,
-            dir: dir
+            dir: dir,
+            vacuum: Cell::new(true)
         }
+    }
+
+    pub fn switch_medium(&self) {
+        match self.vacuum.get() {
+            true => self.vacuum.set(false),
+            false => self.vacuum.set(true)
+        }
+    }
+
+    pub fn in_vacuum(&self) -> bool {
+        self.vacuum.get()
     }
 }
 
