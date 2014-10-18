67ed252d60147b18592f4c25325328326aedc0d8
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-10-18 16:35:45 +0200

    Parse size and depth

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,51 @@
 extern crate rstracer;
 extern crate getopts;
 
-use getopts::{optopt,optflag,getopts,OptGroup};
+use getopts::{Matches, optopt,optflag,getopts,OptGroup, usage};
 use std::os;
 
 use rstracer::RayTracer;
 use rstracer::parser::SceneParser;
 
-fn print_usage(program: &str, _opts: &[OptGroup]) {
-    println!("Usage: {} [options]", program);
-    println!("-h --help\tUsage");
+fn parse_command_line(program: &str, args: &[String], opts: &[OptGroup]) -> Matches {
+    match getopts(args, opts) {
+        Ok(m) => m,
+        Err(f) => {
+            println!("{}", usage(program, opts));
+            fail!(f.to_string())
+        }
+    }
+}
+
+fn get_opt(matches: &Matches, opt: &str, default: uint) -> uint {
+    match matches.opt_str(opt) {
+        Some(opt_str) => from_str::<uint>(opt_str.as_slice()).unwrap_or(default),
+        None => default
+    }
 }
 
 #[allow(dead_code)]
 fn main() {
     let args: Vec<String> = os::args();
 
-    let program = args[0].clone();
-
+    let program = args[0].as_slice();
     let opts = [
-        optflag("h", "help", "print this help menu")
+        optflag("h", "help", "Print this help menu"),
+        optopt("s", "size", "The width and height of the image to be generated", "-s 500"),
+        optopt("d", "depth", "The depth of the recursion in the main loop", "-d 10")
     ];
-    let matches = match getopts(args.tail(), opts) {
-        Ok(m) => { m }
-        Err(f) => { fail!(f.to_string()) }
-    };
+    let matches = parse_command_line(program, args.tail(), opts);
     if matches.opt_present("h") {
-        print_usage(program.as_slice(), opts);
+        println!("{}", usage(program, opts));
         return;
     }
 
+    let size = get_opt(&matches, "s", 100);
+    let depth = get_opt(&matches, "d", 10);
+
     let mut parser = SceneParser::new("scenes/test06.ascii".to_string());
     let scene = parser.parse_scene();
-    let mut tracer = RayTracer::init(250, 250, 10);
+    let mut tracer = RayTracer::init(size, size, depth);
     tracer.set_scene(scene);
     let img = tracer.trace_rays();
     img.save("img.bmp");
