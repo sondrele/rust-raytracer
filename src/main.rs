dc9dae920f18e204f17ba42008cbdf45f54a9a9a
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-10-18 16:44:42 +0200

    Set scene from command line

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,18 @@ fn get_opt(matches: &Matches, opt: &str, default: uint) -> uint {
     }
 }
 
+fn get_str(matches: &Matches, opt: &str, default: &str) -> String {
+    match matches.opt_str(opt) {
+        Some(opt_str) => opt_str,
+        None => default.to_string()
+    }
+}
+
+fn get_scene(matches: &Matches, default: &str) -> String {
+    let name = get_str(matches, "i", default);
+    String::from_str("scenes/") + name + String::from_str(".ascii")
+}
+
 #[allow(dead_code)]
 fn main() {
     let args: Vec<String> = os::args();
@@ -32,7 +44,8 @@ fn main() {
     let opts = [
         optflag("h", "help", "Print this help menu"),
         optopt("s", "size", "The width and height of the image to be generated", "-s 500"),
-        optopt("d", "depth", "The depth of the recursion in the main loop", "-d 10")
+        optopt("d", "depth", "The depth of the recursion in the main loop", "-d 10"),
+        optopt("i", "scene", "The name of a scene located in the ./scenes directory", "-i test01")
     ];
     let matches = parse_command_line(program, args.tail(), opts);
     if matches.opt_present("h") {
@@ -42,8 +55,9 @@ fn main() {
 
     let size = get_opt(&matches, "s", 100);
     let depth = get_opt(&matches, "d", 10);
+    let scene = get_scene(&matches, "test01");
 
-    let mut parser = SceneParser::new("scenes/test06.ascii".to_string());
+    let mut parser = SceneParser::new(scene);
     let scene = parser.parse_scene();
     let mut tracer = RayTracer::init(size, size, depth);
     tracer.set_scene(scene);
