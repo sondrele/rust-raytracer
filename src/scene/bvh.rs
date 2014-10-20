cfcea4b82be2545ab62650b8801d44323cdb517d
Sondre Lefsaker <sondrele@stud.ntnu.no>
2014-10-21 00:13:40 +0200

    Implemented method for building tree. Needs testing and cleanup. When tested, replace Options with a new Node-enum to avoid matching and unwrapping unnessesarily many options

--- src/scene/bvh.rs
+++ src/scene/bvh.rs
@@ -1,7 +1,11 @@
-use vec::Vec3;
-use ray::Ray;
-use scene::shapes;
+use std::cmp;
+
 use scene::shapes::{BoundingBox, Shape};
+
+// pub enum TreeNode<'a> {
+//     Child(Node<'a>),
+//     Empty
+// }
 
 #[derives(Show)]
 pub struct Node<'a> {
@@ -23,32 +27,47 @@ impl<'a> Node<'a> {
         }
     }
 
+    pub fn init(left: Option<Node<'a>>, right: Option<Node<'a>>) -> Node<'a> {
+        let left_bbox = match left {
+            Some(ref n) => n.bbox.unwrap_or(BoundingBox::new()),
+            None => BoundingBox::new()
+        };
+        let right_bbox = match right {
+            Some(ref n) => n.bbox.unwrap_or(BoundingBox::new()),
+            None => BoundingBox::new()
+        };
+
+        let mut node = Node::new();
+        node.left = Some(box left.unwrap());
+        node.right = Some(box right.unwrap());
+        node.bbox = Some(left_bbox + right_bbox);
+        node
+    }
+
     fn add(&mut self, shape: &'a Box<Shape+'a>) {
+        self.bbox = Some(shape.get_bbox());
         self.shape = Some(shape);
-        self.bbox = Some(shape.get_bbox())
     }
 }
 
 pub struct Tree<'a> {
-    dims: uint,
     root: Option<Node<'a>>
 }
 
 impl<'a> Tree<'a> {
     pub fn new() -> Tree<'a> {
         Tree {
-            dims: 3,
             root: None
         }
     }
 
-    pub fn init(&mut self, shapes: &'a Vec<Box<Shape+'a>>) {
+    pub fn init(&mut self, shapes: &'a mut [Box<Shape+'a>]) {
         let depth = 0;
         let root = self.build(shapes, depth);
         self.root = root;
     }
 
-    fn build(&mut self, shapes: &'a Vec<Box<Shape+'a>>, depth: uint) -> Option<Node<'a>> {
+    pub fn build(&mut self, shapes: &'a mut [Box<Shape+'a>], depth: uint) -> Option<Node<'a>> {
         match shapes.len() {
             0 => None,
             1 => {
@@ -58,7 +77,20 @@ impl<'a> Tree<'a> {
                 Some(leaf)
             },
             _ => {
-                fail!("Only size of one so far")
+                let axis = depth as u32 % 3;
+                shapes.sort_by(|a, b| {
+                    match a.get_bbox().centroid()[axis] < b.get_bbox().centroid()[axis] {
+                        true => cmp::Less,
+                        false => cmp::Greater
+                    }
+                });
+                let half = shapes.len() / 2;
+                let (mut head, mut tail) = shapes.split_at_mut(half);
+
+                let left = self.build(head, depth + 1);
+                let right = self.build(tail, depth + 1);
+
+                Some(Node::init(left, right))
             }
         }
     }
@@ -75,9 +107,9 @@ mod tests {
     fn can_init_tree_of_size_1() {
         let sphere = shapes::sphere::Sphere::init(Vec3::init(0.0, 0.0, -5.0), 1.0);
         let shape: Box<Shape> = box sphere;
-        let shapes= vec!(shape);
+        let mut shapes= vec!(shape);
         let mut tree = bvh::Tree::new();
-        tree.init(&shapes);
+        tree.init(shapes.as_mut_slice());
 
         match tree.root {
             Some(node) => assert!(node.leaf == true),
