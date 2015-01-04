72f291a8614db2270ecc285f43c737e0afa2199f
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-01-04 02:30:44 +0100

    Finished converting the scene, might still need some testing for verification

--- src/scene/from_obj/mod.rs
+++ src/scene/from_obj/mod.rs
@@ -1,9 +1,16 @@
 extern crate wavefront_obj;
 
+use std::collections::HashMap;
 use std::borrow::ToOwned;
 use std::io::{BufferedReader, File};
 
 use self::wavefront_obj::{mtl, obj};
+use vec::Vec3;
+use scene;
+use scene::material::{Color, Material};
+use scene::shapes::Primitive;
+use scene::shapes::poly;
+use scene::parser::SceneParser;
 
 fn read_file(path: String) -> String {
     match File::open(&Path::new(path.clone())) {
@@ -26,6 +33,32 @@ fn parse_mtl(file_name: &str) -> mtl::MtlSet {
     }
 }
 
+fn convert_color(c: mtl::Color) -> Color {
+    let mut color = Color::new();
+    color.r(c.r as f32);
+    color.g(c.g as f32);
+    color.b(c.b as f32);
+    color
+}
+
+fn convert_material(mtl: &mtl::Material) -> Material {
+    let mut m = Material::new();
+    m.diffuse = convert_color(mtl.color_diffuse);
+    m.ambient = convert_color(mtl.color_ambient);
+    m.specular = convert_color(mtl.color_specular);
+    m.shininess = mtl.specular_coefficient as f32;
+    m.transparency = mtl.alpha as f32;
+    m
+}
+
+fn convert_materials(mtl_set: mtl::MtlSet) -> HashMap<String, Material> {
+    let mut m = HashMap::with_capacity(mtl_set.materials.len());
+    for mtl in mtl_set.materials.iter() {
+        m.insert(mtl.name.clone(), convert_material(mtl));
+    }
+    m
+}
+
 fn parse_obj(file_name: &str) -> obj::ObjSet {
     let obj_string = read_file(file_name.to_owned());
     match obj::parse(obj_string) {
@@ -34,6 +67,91 @@ fn parse_obj(file_name: &str) -> obj::ObjSet {
     }
 }
 
+fn convert_objects(objects: &Vec<obj::Object>,
+                   mtls: &HashMap<String, Material>) -> Vec<poly::Poly> {
+    let mut polys = Vec::new();
+    for obj in objects.iter() {
+        let ps = convert_object(obj, mtls);
+        polys = polys + ps.as_slice();
+    }
+    polys
+}
+
+fn convert_object(object: &obj::Object, mtls: &HashMap<String, Material>) -> Vec<poly::Poly> {
+    let mut polys = Vec::new();
+    for geo in object.geometry.iter() {
+        let ps = convert_geometry(geo, object, mtls);
+        polys = polys + ps.as_slice();
+    }
+    polys
+}
+
+fn convert_geometry(geometry: &obj::Geometry, object: &obj:: Object,
+                    mtls: &HashMap<String, Material>) -> Vec<poly::Poly> {
+    let m = match &geometry.material_name {
+        &Some(ref name) => mtls.get(name).unwrap(),
+        &None => panic!("No material name associated with Geometry: {}", geometry)
+    };
+
+    let mut polys = Vec::with_capacity(geometry.shapes.len());
+    for shp in geometry.shapes.iter() {
+        match convert_shape(shp, object) {
+            Some(mut poly) => {
+                poly.materials = vec!(m.clone());
+                polys.push(poly);
+            },
+            None => ()
+        }
+    }
+    polys
+}
+
+fn convert_shape(shp: &obj::Shape, object: &obj::Object) -> Option<poly::Poly> {
+    match shp {
+        &obj::Shape::Triangle(vertex_x, vertex_y, vertex_z) => {
+            let mut p = poly::Poly::new();
+            p.vertices[0] = convert_vtindex(vertex_x, object);
+            p.vertices[1] = convert_vtindex(vertex_y, object);
+            p.vertices[2] = convert_vtindex(vertex_z, object);
+            if vertex_x.1 != None {
+                p.vertex_normal = true;
+            }
+            Some(p)
+        },
+        _ => None
+    }
+}
+
+fn convert_vtindex(vt: obj::VTIndex, object: &obj::Object) -> poly::Vertex {
+    let mut vertex = poly::Vertex::new();
+    vertex.position = convert_vertex(object.vertices[vt.0]);
+    match vt.2 {
+        Some(i) => {
+            vertex.normal = convert_vertex(object.normals[i]);
+            vertex.has_normal = true;
+        },
+        None => ()
+    }
+    vertex
+}
+
+fn convert_vertex(v: obj::Vertex) -> Vec3 {
+    Vec3::init(v.x as f32, v.y as f32, v.z as f32)
+}
+
+pub fn parse_obj_scene<'a>(scene_path: String, obj_path: String) -> scene::BvhScene<'a> {
+    let objset = parse_obj(obj_path.as_slice());
+    let mtllib = parse_mtl(objset.material_library.as_slice());
+
+    let materials = convert_materials(mtllib);
+    let polys = convert_objects(&objset.objects, &materials);
+    let prims = polys.map_in_place(|poly| Primitive::Poly(poly));
+
+    let mut parser = SceneParser::new(scene_path);
+    let mut scene = parser.parse_scene();
+    scene.primitives = scene.primitives + prims.as_slice();
+    scene::BvhScene::from_scene(scene)
+}
 
 #[cfg(test)]
 mod test {
@@ -49,7 +167,7 @@ mod test {
 
 
     #[test]
-    fn parse_returns_error() {
+    fn test_parse_returns_error() {
         let e = obj::parse(String::from_str("no_obj"));
 
         match e {
@@ -59,7 +177,7 @@ mod test {
     }
 
     #[test]
-    fn parse_obj_scene() { // Integration
+    fn test_parse_obj_scene() {
         let obj_file =
 r#"
 # Blender v2.71 (sub 0) OBJ File: 'cube.blend'
@@ -161,6 +279,11 @@ illum 2
         let obj = parse_obj(path.as_slice());
         assert_eq!(obj.material_library.as_slice(), "cube.mtl");
         assert_eq!(obj.objects.len(), 1)
+    }
+
+    #[test]
+    fn parse_and_convert_mtl() {
+
     }
 
 }
