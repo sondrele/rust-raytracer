2adfa7641579afc15154fae5e9f19248f91785e8
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-01-07 12:32:41 +0100

    Make Mesh contain PolyIndices, poviding a function to generate the Poly's with references to the data on the fly

--- src/scene/shapes/poly_mesh.rs
+++ src/scene/shapes/poly_mesh.rs
@@ -1,75 +1,115 @@
 use std::num::FloatMath;
-use std::ops::Index;
 
 use vec::Vec3;
 use ray::Ray;
 use scene::material::{Material, Color};
 use scene::shapes::{BoundingBox, Shape, ShapeIntersection};
 
-type RefVec<'a> = &'a Vec3;
-
 // Index into the 'vertices' Vec in PolyMesh
-pub type VertexIndex<'a> = &'a Vec3;
+// pub type VertexIndex<'a> = &'a Vec3;
+type VertexIndex = uint;
 
 // Index into the 'normals' Vec in PolyMesh
-pub type NormalIndex<'a> = &'a Vec3;
+// pub type NormalIndex<'a> = &'a Vec3;
+type NormalIndex = uint;
 
 // Index into the 'materials' Vec in PolyMesh
-pub type MaterialIndex<'a> = &'a Material;
+// pub type MaterialIndex<'a> = &'a Material;
+type MaterialIndex = uint;
 
-pub type PolyVertex<'a> = (VertexIndex<'a>, Option<NormalIndex<'a>>, Option<MaterialIndex<'a>>);
+type PolyVertex = (VertexIndex, Option<NormalIndex>, Option<MaterialIndex>);
 
-pub struct PolyMesh<'a> {
+type PolyIndex = (PolyVertex, PolyVertex, PolyVertex);
+
+pub struct Poly<'a>{
+    pub x: (&'a Vec3, Option<&'a Vec3>, Option<&'a Material>),
+    pub y: (&'a Vec3, Option<&'a Vec3>, Option<&'a Material>),
+    pub z: (&'a Vec3, Option<&'a Vec3>, Option<&'a Material>),
+}
+
+pub struct PolyMesh {
     pub vertices: Vec<Vec3>,
     pub normals: Vec<Vec3>,
     pub materials: Vec<Material>,
-    pub polys: Vec<Poly<'a>>
+    poly_indices: Vec<PolyIndex>
 }
 
-impl<'a> PolyMesh<'a> {
-    pub fn new() -> PolyMesh<'a> {
+impl PolyMesh {
+    pub fn new() -> PolyMesh {
         PolyMesh {
-            materials: Vec::new(),
             vertices: Vec::new(),
             normals: Vec::new(),
-            polys: Vec::new()
+            materials: Vec::new(),
+            poly_indices: Vec::new()
         }
     }
-}
 
-pub struct Poly<'a> {
-    pub x: PolyVertex<'a>,
-    pub y: PolyVertex<'a>,
-    pub z: PolyVertex<'a>
-}
+    pub fn get_poly(&self, index: uint) -> Poly {
+        let p = &self.poly_indices[index];
+        match p {
+            &((xv, Some(xn), Some(xm)), (yv, Some(yn), Some(ym)), (zv, Some(zn), Some(zm))) => {
+                Poly {
+                    x: (&self.vertices[xv], Some(&self.normals[xn]), Some(&self.materials[xm])),
+                    y: (&self.vertices[yv], Some(&self.normals[yn]), Some(&self.materials[ym])),
+                    z: (&self.vertices[zv], Some(&self.normals[zn]), Some(&self.materials[zm]))
+                }
+            },
+            &((xv, Some(xn), None), (yv, Some(yn), None), (zv, Some(zn), None)) => {
+                Poly {
+                    x: (&self.vertices[xv], Some(&self.normals[xn]), None),
+                    y: (&self.vertices[yv], Some(&self.normals[yn]), None),
+                    z: (&self.vertices[zv], Some(&self.normals[zn]), None)
+                }
+            },
+            &((xv, None, Some(xm)), (yv, None, Some(ym)), (zv, None, Some(zm))) => {
+                Poly {
+                    x: (&self.vertices[xv], None, Some(&self.materials[xm])),
+                    y: (&self.vertices[yv], None, Some(&self.materials[ym])),
+                    z: (&self.vertices[zv], None, Some(&self.materials[zm]))
+                }
+            },
+            &((xv, None, None), (yv, None, None), (zv, None, None)) => {
+                Poly {
+                    x: (&self.vertices[xv], None, None),
+                    y: (&self.vertices[yv], None, None),
+                    z: (&self.vertices[zv], None, None)
+                }
+            },
+            _ => panic!("Invalid PolyIndex")
+        }
+    }
 
-impl<'a> Index<u32> for Poly<'a> {
-    type Output = Vec3;
+    pub fn intersects(&self, ray: &Ray) -> ShapeIntersection {
+        let mut point = 0.0;
 
-    fn index<'b>(&'b self, index: &u32) -> &'b Vec3 {
-        match index {
-            &0 => self.x.0,
-            &1 => self.y.0,
-            &2 => self.z.0,
-            _ => panic!("Index out of bound: {}", index)
+        let mut has_intersected = false;
+        for i in range(0, self.poly_indices.len()) {
+            let p = self.get_poly(i);
+            match p.intersects(ray) {
+                ShapeIntersection::Hit(pt) if !has_intersected => {
+                    point = pt;
+                    has_intersected = true;
+                }
+                ShapeIntersection::Hit(pt) if has_intersected && pt < point => {
+                    point = pt;
+                },
+                _ => ()
+            }
+        }
+        match has_intersected {
+            true => ShapeIntersection::Hit(point),
+            false => ShapeIntersection::Missed
         }
     }
 }
 
 impl<'a> Poly<'a> {
-    pub fn new(x: &'a Vec3, y: &'a Vec3, z: &'a Vec3) -> Poly<'a> {
-        Poly {
-            x: (x, None, None),
-            y: (y, None, None),
-            z: (z, None, None)
-        }
-    }
 
     fn weighted_areas(&self, point: Vec3) -> (f32, f32, f32) {
-        let area = Vec3::get_area(self[0], self[1], self[2]);
-        let area0 = Vec3::get_area(self[0], self[1], point) / area;
-        let area1 = Vec3::get_area(self[2], self[0], point) / area;
-        let area2 = Vec3::get_area(self[1], self[2], point) / area;
+        let area = Vec3::get_area(*self.x.0, *self.y.0, *self.z.0);
+        let area0 = Vec3::get_area(*self.x.0, *self.y.0, point) / area;
+        let area1 = Vec3::get_area(*self.z.0, *self.x.0, point) / area;
+        let area2 = Vec3::get_area(*self.y.0, *self.z.0, point) / area;
 
         if area0 > 1.0 || area1 > 1.0 || area2 > 1.0 {
             panic!("Cannot get area, as point is outside of poly")
@@ -90,8 +130,8 @@ impl<'a> Poly<'a> {
     }
 
     fn static_normal(&self) -> Vec3 {
-        let v = self[1] - self[0];
-        let w = self[2] - self[0];
+        let v = *self.y.0 - *self.x.0;
+        let w = *self.z.0 - *self.x.0;
         let mut norm = v.cross(w);
         norm.normalize();
         norm
@@ -113,15 +153,15 @@ impl<'a> Poly<'a> {
 impl<'a> Shape for Poly<'a> {
     fn get_bbox(&self) -> BoundingBox {
         let min = Vec3::init(
-            self[0][0].min(self[1][0].min(self[2][0])),
-            self[0][1].min(self[1][1].min(self[2][1])),
-            self[0][2].min(self[1][2].min(self[2][2]))
+            self.x.0[0].min(self.y.0[0].min(self.z.0[0])),
+            self.x.0[1].min(self.y.0[1].min(self.z.0[1])),
+            self.x.0[2].min(self.y.0[2].min(self.z.0[2]))
         );
 
         let max = Vec3::init(
-            self[0][0].max(self[1][0].max(self[2][0])),
-            self[0][1].max(self[1][1].max(self[2][1])),
-            self[0][2].max(self[1][2].max(self[2][2]))
+            self.x.0[0].max(self.y.0[0].max(self.z.0[0])),
+            self.x.0[1].max(self.y.0[1].max(self.z.0[1])),
+            self.x.0[2].max(self.y.0[2].max(self.z.0[2]))
         );
 
         BoundingBox::init(min, max)
@@ -130,9 +170,9 @@ impl<'a> Shape for Poly<'a> {
     fn intersects(&self, ray: &Ray) -> ShapeIntersection {
         let p: Vec3 = ray.ori;
         let d: Vec3 = ray.dir;
-        let v0: Vec3 = self[0];
-        let v1: Vec3 = self[1];
-        let v2: Vec3 = self[2];
+        let v0: Vec3 = *self.x.0;
+        let v1: Vec3 = *self.y.0;
+        let v2: Vec3 = *self.z.0;
 
         let e1: Vec3 = v1 - v0;
         let e2: Vec3 = v2 - v0;
@@ -173,7 +213,7 @@ impl<'a> Shape for Poly<'a> {
     fn get_material(&self) -> Material {
         match self.x.2 {
             Some(material) => material.clone(),
-            None => panic!("Poly not associated with a material")
+            None => panic!("PolyIndex not associated with a material")
         }
     }
 
@@ -194,5 +234,52 @@ impl<'a> Shape for Poly<'a> {
             (Some(_), Some(_), Some(_)) => self.interpolated_color(point),
             _ => self.get_material().diffuse
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use vec::Vec3;
+    use ray::Ray;
+    use scene::shapes::ShapeIntersection;
+    use scene::shapes::poly_mesh::{Poly, PolyMesh};
+
+    fn create_mesh() -> PolyMesh {
+        let mut m = PolyMesh::new();
+        m.vertices = vec!(
+            Vec3::init(0.0, 0.0, 0.0), Vec3::init(2.0, 0.0, 0.0), Vec3::init(0.0, 2.0, 0.0),
+            Vec3::init(0.0, 0.0, -2.0), Vec3::init(4.0, 0.0, -2.0), Vec3::init(0.0, 4.0, -2.0),
+            Vec3::init(0.0, 0.0, -1.0), Vec3::init(2.0, 0.0, -1.0), Vec3::init(0.0, 2.0, -1.0),
+            Vec3::init(0.0, 0.0, -3.0), Vec3::init(6.0, 0.0, -3.0), Vec3::init(0.0, 6.0, -3.0)
+        );
+        m.poly_indices = vec!(
+            ((0, None, None), (1, None, None), (2, None, None)),
+            ((3, None, None), (4, None, None), (5, None, None)),
+            ((6, None, None), (7, None, None), (8, None, None)),
+            ((9, None, None), (10, None, None), (11, None, None))
+        );
+        m
+    }
+
+    #[test]
+    fn create_poly_from_polymesh() {
+        let mesh = create_mesh();
+        let p: Poly = mesh.get_poly(0);
+
+        assert_eq!(&mesh.vertices[0], p.x.0);
+        assert_eq!(&mesh.vertices[1], p.y.0);
+        assert_eq!(&mesh.vertices[2], p.z.0);
+    }
+
+    #[test]
+    fn can_intersect_polymesh() {
+        let mesh = create_mesh();
+
+        let ray = Ray::init(Vec3::init(-1.5, 1.0, 1.0), Vec3::init(1.0, 0.0, -1.0));
+        match mesh.intersects(&ray) {
+            ShapeIntersection::Hit(x) => assert_eq!(x, 2.0),
+            ShapeIntersection::Missed => panic!("Missed mesh")
+        }
+
     }
 }
